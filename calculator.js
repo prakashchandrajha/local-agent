32fb91bf0bc16b6a28dedda03f0e3e1bfc8598c3
prakash <prakash@email.com>
2026-03-06 10:28:03 +0530

    q

--- calculator.js
+++ calculator.js
@@ -1,14 +1,27 @@
+// Calculator module
+
+function add(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Both arguments must be numbers');
+  }
+  return a + b;
+}
+
+function subtract(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Both arguments must be numbers');
+  }
+  return a - b;
+}
+
 function divide(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new Error('Both arguments must be numbers');
   }
-  
-  // Check for division by zero
   if (b === 0) {
-    throw new Error('Cannot divide by zero');
+    throw new Error('Division by zero is not allowed');
   }
-  
   return a / b;
 }
 
-module.exports = { divide };
+module.exports = { add, subtract, divide };
